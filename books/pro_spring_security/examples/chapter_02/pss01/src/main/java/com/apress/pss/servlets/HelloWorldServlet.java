ea0fe154cf80da076a836ec037ee0ce5e6764365
Eric Jankowski <eric.jankowski@mozido.com>
2016-01-28 11:10:29 -0600

    putting Spring Security on hold for a bit while I concentrate on OCP Java SE 7

---
+++ books/pro_spring_security/examples/chapter_02/pss01/src/main/java/com/apress/pss/servlets/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package com.apress.pss.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+        try{
+            response.getWriter().write("Hello World");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
