1f4960cf2a322a8b2caab29045d92531c64d7be2
Eric Jankowski <eric.jankowski@mozido.com>
2016-03-22 09:56:40 -0500

    return types, covariant returns, overriding, and overloading

--- books/OCP_Java_SE_7_Programmer/Code_Examples/src/main/java/com/ericjankowski/study/java/se7/polymorphismo/TestAnimals.java
+++ books/OCP_Java_SE_7_Programmer/Code_Examples/src/main/java/com/ericjankowski/study/java/se7/polymorphismo/TestAnimals.java
@@ -6,5 +6,22 @@ public class TestAnimals {
         Animal b = new Horse();
         a.eat();
         b.eat();
+        
+        TestAnimals test = new TestAnimals();
+        Animal animal = new Animal();
+        Horse horse = new Horse();
+        test.doStuff(animal);
+        test.doStuff(horse);
+        
+        Animal animalReferenceToHorse = new Horse();
+        test.doStuff(animalReferenceToHorse);
+    }
+    
+    public void doStuff(Animal a){
+        System.out.println("This is the Animal version");
+    }
+    
+    public void doStuff(Horse h){
+        System.out.println("This is the Horse version");
     }
 }
