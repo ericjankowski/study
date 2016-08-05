1375124f2ae21225a6f47ee283feb08ea67a49a1
Eric Jankowski <eric.r.jankowski@gmail.com>
2016-08-05 08:25:37 -0500

    started BinaryTree implementation

---
+++ books/introduction_to_algorithms/src/main/java/com/ericjankowski/study/clrs/datastructures/dynamic/BinaryTree.java
@@ -0,0 +1,54 @@
+package com.ericjankowski.study.clrs.datastructures.dynamic;
+
+
+public class BinaryTree {
+    
+    TreeNode root;
+    int size = 0;
+    
+    public void insert(Object value){
+        TreeNode node = new TreeNode(value);
+        insert(node, root);
+    }
+    
+    private void insert(TreeNode nodeToInsert, TreeNode treeNode) {
+        if(treeNode == null){
+            root = nodeToInsert;
+        }else if(treeNode.leftChild == null){
+            treeNode.leftChild = nodeToInsert;
+            nodeToInsert.parent = treeNode;
+        }else if(treeNode.rightChild == null){
+            treeNode.rightChild = nodeToInsert;
+            nodeToInsert.parent = treeNode;
+        }else{
+            int coinFlip = (int)((Math.random()*100000000)%2);
+            if(coinFlip == 0){
+                insert(nodeToInsert, treeNode.leftChild);
+            }else{
+                insert(nodeToInsert, treeNode.rightChild);
+            }
+        }
+        size++;
+    }
+    
+    public int size() {
+        return size;
+    }
+
+    class TreeNode{
+        Object key;
+        TreeNode parent;
+        TreeNode leftChild;
+        TreeNode rightChild;
+        
+        TreeNode(Object key){
+            this.key = key;
+        }
+        
+        public String toString(){
+            return key.toString();
+        }
+    }
+
+    
+}
---
+++ books/introduction_to_algorithms/src/test/java/com/ericjankowski/study/clrs/datastructures/dynamic/BinaryTreeTest.java
@@ -0,0 +1,27 @@
+package com.ericjankowski.study.clrs.datastructures.dynamic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinaryTreeTest {
+
+    @Test
+    public void insertIncreasesSize() {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert("Foo");
+
+        assertEquals(1, tree.size());
+    }
+    
+    @Test
+    public void insertIncreasesSizeCorrectly() {
+        BinaryTree tree = new BinaryTree();
+        
+        tree.insert("Foo");
+        tree.insert("Bar");
+        
+        assertEquals(2, tree.size());
+    }
+}
