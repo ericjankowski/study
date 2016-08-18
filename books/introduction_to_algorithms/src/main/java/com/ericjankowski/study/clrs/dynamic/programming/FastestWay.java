76ad3f93ff779cb66d6d03e779af3aa7a92b0f2b
Eric Jankowski <eric.r.jankowski@gmail.com>
2016-08-18 15:14:29 -0500

    fastest way renaming some variables

--- books/introduction_to_algorithms/src/main/java/com/ericjankowski/study/clrs/dynamic/programming/FastestWay.java
+++ books/introduction_to_algorithms/src/main/java/com/ericjankowski/study/clrs/dynamic/programming/FastestWay.java
@@ -3,40 +3,40 @@ package com.ericjankowski.study.clrs.dynamic.programming;
 public class FastestWay {
     public static void main(String[] args) {
         int n = 6;
-        int [][] a = {{7,9,3,4,8,4},{8,5,6,4,5,7}};
-        int [][] t = {{2,3,1,3,4},{2,1,2,2,1}};
-        int [] e = {2,4};
-        int [] x = {3,2};
-        int [][] f = {{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1}};
-        int [][] l = {{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1}};
+        int [][] timeAtStation = {{7,9,3,4,8,4},{8,5,6,4,5,7}};
+        int [][] timeToSwitchLines = {{2,3,1,3,4},{2,1,2,2,1}};
+        int [] entranceTime = {2,4};
+        int [] exitTime = {3,2};
+        int [][] timeSpent = {{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1}};
+        int [][] paths = {{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1}};
         int fastestTime = -1;
         int fastestPath = -1;
         
-        f[0][0] = e[0] + a[0][0];
-        f[1][0] = e[1] + a[1][0];
+        timeSpent[0][0] = entranceTime[0] + timeAtStation[0][0];
+        timeSpent[1][0] = entranceTime[1] + timeAtStation[1][0];
         
         for(int i = 1; i<n; i++){
-            if(f[0][i-1] + a[0][i] <= f[1][i-1] + t[1][i-1]+a[0][i]){
-                f[0][i] = f[0][i-1] + a[0][i];
-                l[0][i] = 1;
+            if(timeSpent[0][i-1] + timeAtStation[0][i] <= timeSpent[1][i-1] + timeToSwitchLines[1][i-1]+timeAtStation[0][i]){
+                timeSpent[0][i] = timeSpent[0][i-1] + timeAtStation[0][i];
+                paths[0][i] = 1;
             }else{
-                f[0][i] = f[1][i-1] + t[1][i-1]+ a[0][i];
-                l[0][i] = 2;
+                timeSpent[0][i] = timeSpent[1][i-1] + timeToSwitchLines[1][i-1]+ timeAtStation[0][i];
+                paths[0][i] = 2;
             }
             
-            if(f[1][i-1] + a[1][i] <= f[0][i-1] + t[0][i-1] + a[1][i]){
-                f[1][i] = f[1][i-1] + a[1][i];
-                l[1][i] = 2;
+            if(timeSpent[1][i-1] + timeAtStation[1][i] <= timeSpent[0][i-1] + timeToSwitchLines[0][i-1] + timeAtStation[1][i]){
+                timeSpent[1][i] = timeSpent[1][i-1] + timeAtStation[1][i];
+                paths[1][i] = 2;
             }else{
-                f[1][i] = f[0][i-1] + t[0][i-1] + a[1][i];
-                l[1][i] = 1;
+                timeSpent[1][i] = timeSpent[0][i-1] + timeToSwitchLines[0][i-1] + timeAtStation[1][i];
+                paths[1][i] = 1;
             }
         }
-        if(f[0][n-1] + x[0] <= f[1][n-1] + x[1]){
-            fastestTime = f[0][n-1] + x[0];
+        if(timeSpent[0][n-1] + exitTime[0] <= timeSpent[1][n-1] + exitTime[1]){
+            fastestTime = timeSpent[0][n-1] + exitTime[0];
             fastestPath = 1;
         }else{
-            fastestTime = f[1][n-1] + x[1];
+            fastestTime = timeSpent[1][n-1] + exitTime[1];
             fastestPath = 2;
         }
         
@@ -44,7 +44,7 @@ public class FastestWay {
         int i = fastestPath;
         System.out.println("line: " + i + " station: " + n);
         for(int j = n-1; j>0; j--){
-            System.out.println("line: " + l[fastestPath][j] + " station: " + (j));
+            System.out.println("line: " + paths[fastestPath][j] + " station: " + (j));
         }
     }
 }
