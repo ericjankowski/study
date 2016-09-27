2188c82a28f99337c61845292510262771946e8c
Eric Jankowski <eric.r.jankowski@gmail.com>
2016-09-27 09:16:57 -0500

    java certification chapter 2

--- books/introduction_to_algorithms/src/main/java/com/ericjankowski/study/clrs/graph/algorithms/BreadthFirstSearch.java
+++ books/introduction_to_algorithms/src/main/java/com/ericjankowski/study/clrs/graph/algorithms/BreadthFirstSearch.java
@@ -4,21 +4,21 @@ import com.ericjankowski.study.clrs.datastructures.dynamic.SinglyLinkedList;
 
 public class BreadthFirstSearch {
     public static void main(String[] args) {
-        SinglyLinkedList [] adjacencyList = {new SinglyLinkedList(), new SinglyLinkedList(), new SinglyLinkedList(), new SinglyLinkedList(), new SinglyLinkedList()};
-        adjacencyList[0].insert(2);
-        adjacencyList[0].insert(5);
-        adjacencyList[1].insert(1);
-        adjacencyList[1].insert(5);
-        adjacencyList[1].insert(3);
-        adjacencyList[1].insert(4);
-        adjacencyList[2].insert(2);
-        adjacencyList[2].insert(4);
-        adjacencyList[3].insert(2);
-        adjacencyList[3].insert(5);
-        adjacencyList[3].insert(3);
-        adjacencyList[4].insert(4);
-        adjacencyList[4].insert(1);
-        adjacencyList[4].insert(2);
+        SinglyLinkedList [] graph = {new SinglyLinkedList(), new SinglyLinkedList(), new SinglyLinkedList(), new SinglyLinkedList(), new SinglyLinkedList()};
+        graph[0].insert(2);
+        graph[0].insert(5);
+        graph[1].insert(1);
+        graph[1].insert(5);
+        graph[1].insert(3);
+        graph[1].insert(4);
+        graph[2].insert(2);
+        graph[2].insert(4);
+        graph[3].insert(2);
+        graph[3].insert(5);
+        graph[3].insert(3);
+        graph[4].insert(4);
+        graph[4].insert(1);
+        graph[4].insert(2);
     }
     
 }
