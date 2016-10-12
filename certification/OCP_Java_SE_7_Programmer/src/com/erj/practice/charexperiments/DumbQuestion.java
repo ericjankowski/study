4ecb5274162b0637d5fe039dc5aef1f6d86f4481
Eric Jankowski <eric.r.jankowski@gmail.com>
2016-10-12 08:03:12 -0500

    finished with chapter 5

---
+++ certification/OCP_Java_SE_7_Programmer/src/com/erj/practice/charexperiments/DumbQuestion.java
@@ -0,0 +1,8 @@
+package com.erj.practice.charexperiments;
+
+public class DumbQuestion {
+    public static void main(String[] args) {
+        char[] ca = {0x3e, \u004e, 78};
+        System.out.println((ca[0]==ca[1]) + " " + (ca[0]==ca[2]));
+    }
+}
