eef141fd62ff7fe53eecf98aef8550c04b5a4e72
Eric Jankowski <eric.r.jankowski@gmail.com>
2016-10-13 14:13:43 -0500

    OCP chapter 6 complete

--- certification/OCP_Java_SE_7_Programmer/src/com/erj/practice/charexperiments/DumbQuestion.java
+++ certification/OCP_Java_SE_7_Programmer/src/com/erj/practice/charexperiments/DumbQuestion.java
@@ -4,5 +4,6 @@ public class DumbQuestion {
     public static void main(String[] args) {
         char[] ca = {0x3e, \u004e, 78};
         System.out.println((ca[0]==ca[1]) + " " + (ca[0]==ca[2]));
+        
     }
 }
