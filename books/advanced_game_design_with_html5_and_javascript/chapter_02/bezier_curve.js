836068bf4297d489f76bd4a48a689f4e82943b46
Eric Jankowski <eric.r.jankowski@gmail.com>
2017-09-19 10:45:54 -0400

    quadratic and bezier curves

---
+++ books/advanced_game_design_with_html5_and_javascript/chapter_02/bezier_curve.js
@@ -0,0 +1,13 @@
+let canvas = document.createElement("canvas");
+canvas.setAttribute("width", "256");
+canvas.setAttribute("height", "256");
+canvas.style.border = "1px dashed black";
+document.body.appendChild(canvas);
+let ctx = canvas.getContext("2d");
+
+ctx.stokeStyle = "black";
+ctx.lineWidth = 3;
+ctx.beginPath();
+ctx.moveTo(32, 128);
+ctx.bezierCurveTo(32, 20, 224, 20, 224, 128);
+ctx.stroke();
