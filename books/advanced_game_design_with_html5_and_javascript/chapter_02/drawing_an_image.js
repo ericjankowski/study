46fa2a66ef527ba5a012d4a60884a5363f49e5dc
Eric Jankowski <eric.r.jankowski@gmail.com>
2017-09-21 10:57:18 -0400

    drawing an image on a canvas

---
+++ books/advanced_game_design_with_html5_and_javascript/chapter_02/drawing_an_image.js
@@ -0,0 +1,16 @@
+let canvas = document.createElement("canvas");
+canvas.setAttribute("width", "256");
+canvas.setAttribute("height", "256");
+canvas.style.border = "1px dashed black";
+document.body.appendChild(canvas);
+let ctx = canvas.getContext("2d");
+
+let basketballImage = new Image();
+basketballImage.addEventListener("load", loadHandler, false);
+basketballImage.src = "../images/basketball.png";
+basketballImage.style.width = "10%";
+basketballImage.style.height = "auto";
+
+function loadHandler(){
+	ctx.drawImage(basketballImage, 0, 0);
+}
