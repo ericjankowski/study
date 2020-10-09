0f164c26d6a37a3cd626fbcc71eee3fb9b2618cb
Eric Jankowski <eric.r.jankowski@gmail.com>
2020-10-09 15:05:10 -0400

    blazor

---
+++ books/pro_aspnet_core_3/SportsSln/SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using SportsStore.Infrastructure;
+using SportsStore.Models.ViewModels;
+using Xunit;
+namespace SportsStore.Tests
+{
+    public class PageLinkTagHelperTests
+    {
+        [Fact]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange
+            var mockUrlHelper = new Mock<IUrlHelper>();
+            mockUrlHelper.SetupSequence(x => x.Action(It.IsAny<UrlActionContext>()))
+                .Returns("Test/Page1")
+                .Returns("Test/Page2")
+                .Returns("Test/Page3");
+            var mockUrlHelperFactory = new Mock<IUrlHelperFactory>();
+            mockUrlHelperFactory.Setup(f =>
+                    f.GetUrlHelper(It.IsAny<ActionContext>()))
+                        .Returns(mockUrlHelper.Object);
+            PageLinkTagHelper helper =
+                    new PageLinkTagHelper(mockUrlHelperFactory.Object)
+                    {
+                        PageModel = new PagingInfo
+                        {
+                            CurrentPage = 2,
+                            TotalItems = 28,
+                            ItemsPerPage = 10
+                        },
+                        PageAction = "Test"
+                    };
+            TagHelperContext ctx = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), "");
+            var mockTagHelperContentcontent = new Mock<TagHelperContent>();
+            TagHelperOutput output = new TagHelperOutput("div",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(mockTagHelperContentcontent.Object));
+            // Act
+            helper.Process(ctx, output);
+            // Assert
+            Assert.Equal(@"<a href=""Test/Page1"">1</a>"
+                + @"<a href=""Test/Page2"">2</a>"
+                + @"<a href=""Test/Page3"">3</a>",
+                 output.Content.GetContent());
+        }
+    }
+}
